da2cd1346b96a36227619a13b47e9d191311a147
Mattéo Donatelli <matteo.donatelli1@gmail.com>
2024-02-01 14:29:04 +0100

    PageBesoin updated

--- front/src/pages/Needs.jsx
+++ front/src/pages/Needs.jsx
@@ -1,17 +1,28 @@
+import { useEffect, useState } from "react";
+
+import Carousel from "./../components/CarouselComponent.jsx"
+
 import Logo from "./../assets/Logo.png"
 
 import "./Needs.css";
 
 function Needs() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("")
+    .then(res => res.json)
+    .then(data => setData(data))
+  }, [])
+
   return (
     <section id="needs">
       <img src={Logo} alt="Logo" />
       <h1>J'ai soif !</h1>
-      {/* <Carousel /> */}
-      <div id="carousel"></div>
+      <Carousel data={data}/>
       <div id="dots">
         <div className="dot"></div>
-        <div className="dot"></div>
+        <div className="dot selected"></div>
         <div className="dot"></div>
         <div className="dot"></div>
       </div>
