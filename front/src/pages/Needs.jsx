e7fad307eecfa06a3ea2e16dd9495b94f4906fbf
Paul-Lac <plnlacroix@gmail.com>
2024-02-01 23:41:51 +0100

    add modal button and modal card

--- front/src/pages/Needs.jsx
+++ front/src/pages/Needs.jsx
@@ -1,35 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
-import CarouselComponent from "./../components/CarouselComponent.jsx"
+import CarouselComponent from "./../components/CarouselComponent.jsx";
 
 import "./Needs.css";
 
-function Needs() {  
+function Needs() {
   const { id } = useLoaderData();
 
   const [carouselData, setCarouselData] = useState([]);
   const [objective, setObjective] = useState("");
+  const [solution, setSolution] = useState("");
+  const [buttonVisible, setButtonVisible] = useState(false);
+  const [isModalClosed, setIsModalClosed] = useState(true);
 
-  useEffect(()=> {
+  useEffect(() => {
     fetch("http://localhost:4242/db")
-    .then(res => res.json())
-    .then(data => {
-      setCarouselData(data[id].instructions);
-      setObjective(data[id].objective);
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCarouselData(data[id].instructions);
+        setObjective(data[id].objective);
+        setSolution(data[id].solution);
+      });
   }, []);
 
+  setTimeout(() => {
+    setButtonVisible(true);
+  }, 5000);
+
+  const handleBtnCLick = () => {
+    setIsModalClosed(false);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalClosed(true);
+  };
+
   return (
-    <section id="needs">
-      <CarouselComponent carouselData={carouselData} objective={objective} />
-      {/* <div id="dots">
+    <>
+      <section id="needs">
+        <div
+          className={`needs-overlay ${isModalClosed ? "modal-closed" : ""}`}
+        />
+        <div className={`card-modal ${isModalClosed ? "modal-closed" : ""}`}>
+          <img src="" />
+          <h2>Test</h2>
+          <button type="button" onClick={handleCloseModal}>
+            Fermer
+          </button>
+        </div>
+        <CarouselComponent carouselData={carouselData} objective={objective} />
+        {/* <div id="dots">
         <div className="dot"></div>
         <div className="dot selected"></div>
         <div className="dot"></div>
         <div className="dot"></div>
       </div> */}
-    </section>
+        <button
+          className={`btn-modal ${buttonVisible ? "btn-modal-visible" : ""}`}
+          onClick={handleBtnCLick}
+        >
+          Envie d'une autre vie ?
+        </button>
+      </section>
+    </>
   );
 }
 
