8b744104147e8b2849132a798a0b1773bfa496b6
Gregori Schultz <gregorischultz07@gmail.com>
2024-02-01 18:21:01 +0100

    add img

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,17 +1,18 @@
-import image3 from "../assets/rechauffer.png"
+import image1 from "../assets/nourriture.png";
 import ButtonNav from "../components/ButtonNav";
-
-
+import image2 from "../assets/boisson.png";
+import image3 from "../assets/feu.png";
+import image4 from "../assets/repos.png";
 
 
 function Home() {
   return (
     <div className="home-page">
     <div className="icon-container">
-      <ButtonNav linkURL="/nourriture" imgSrc={image3} altImg="nourriture"/>
-      <ButtonNav linkURL="/boisson" imgSrc={image3} altImg="boisson"/>
+      <ButtonNav linkURL="/nourriture" imgSrc={image1} altImg="nourriture"/>
+      <ButtonNav linkURL="/boisson" imgSrc={image2} altImg="boisson"/>
       <ButtonNav linkURL="/rechauffage" imgSrc={image3} altImg="rechauffage"/>
-      <ButtonNav linkURL="/repo" imgSrc={image3} altImg="repo"/>
+      <ButtonNav linkURL="/repo" imgSrc={image4} altImg="repo"/>
       
     </div>
   </div>
