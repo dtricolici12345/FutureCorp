cb7761410431690126ccf82b06eef29e52a96b2d
Mattéo Donatelli <matteo.donatelli1@gmail.com>
2024-02-01 14:16:19 +0100

    First version

--- front/src/pages/Needs.jsx
+++ front/src/pages/Needs.jsx
@@ -1,5 +1,22 @@
+import Logo from "./../assets/Logo.png"
+
+import "./Needs.css";
+
 function Needs() {
-  return <>Je suis Needs</>;
+  return (
+    <section id="needs">
+      <img src={Logo} alt="Logo" />
+      <h1>J'ai soif !</h1>
+      {/* <Carousel /> */}
+      <div id="carousel"></div>
+      <div id="dots">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+    </section>
+  );
 }
 
 export default Needs;
